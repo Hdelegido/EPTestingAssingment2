811faa10fdd2dbce1d6768980e2dde7ed7dc9a27
Hdelegido <hectordelegido2001@gmail.com>
2022-12-27 23:17:47 +0100

    PasswordInvalidFormatException.java created

---
+++ src/exceptions/PasswordInvalidFormatException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class PasswordInvalidFormatException extends Exception {
+
+    public PasswordInvalidFormatException(String message) {
+        super(message);
+    }
+
+}
