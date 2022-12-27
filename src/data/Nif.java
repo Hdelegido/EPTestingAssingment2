73e2fdf875e931d37866c3621186fa814d1b573c
Hdelegido <hectordelegido2001@gmail.com>
2022-12-27 23:13:49 +0100

    Nif modified

--- src/data/Nif.java
+++ src/data/Nif.java
@@ -1,39 +1,45 @@
 package data;
+
+import exceptions.NifInvalidFormatException;
+
 /**
  * Essential data classes
  */
 final public class Nif {
     // The tax identification number in the Spanish state.
     private final String nif;
-    public Nif(String code) {
-     if (code == null){
-         throw new NullPointerException("Parameter is null.");
-     }else if(!isValidNif(code) ){
-         throw new IllegalArgumentException();
-     }else this.nif = code;
+
+    public Nif(String code) throws NifInvalidFormatException {
+        if (code == null) throw new NullPointerException("Nif can't be null");
+        if (!validFormat(code)) throw new NifInvalidFormatException("Nif must consist of 8 digits + 1 letter");
+        this.nif = code;
     }
-    public boolean isValidNif(String nif){
-        if (nif.length()!=9 || !Nif8Num1Letter(nif) ){
-            return false;
-        }else return true;
+
+    private boolean validFormat(String code) {
+        if (code.length() != 9) return false;
+        boolean validDigits = code.substring(0, code.length() - 1).chars().allMatch(Character::isDigit);
+        return validDigits && Character.isLetter(code.charAt(code.length() - 1));
     }
-    public boolean Nif8Num1Letter(String nif){
-        if (nif.substring(0,7).matches("[0-9]*") & nif.substring(8).matches("[A-Z]*")){
-            return true;
-        }else return false;
+
+    public String getNif() {
+        return nif;
     }
-    public String getNif () { return nif; }
+
     @Override
-    public boolean equals (Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Nif niff = (Nif) o;
-        return nif.equals(niff.nif);
+        Nif otherNiff = (Nif) o;
+        return nif.equals(otherNiff.nif);
     }
+
     @Override
-    public int hashCode () { return nif.hashCode(); }
+    public int hashCode() {
+        return nif.hashCode();
+    }
+
     @Override
-    public String toString () {
-        return "Nif{" + "nif ciudadano='" + nif + '\'' + '}';
+    public String toString() {
+        return String.format("Nif{nif='%s'}", nif);
     }
 }
